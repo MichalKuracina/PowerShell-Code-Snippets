a2bf7817817a8e4f8d479bbcdd0de5f732e86c39
Michal <31184485+MichalKuracina@users.noreply.github.com>
2022-10-20 17:53:15 +0200

    Add files via upload

---
+++ log.ps1
@@ -0,0 +1,27 @@
+﻿
+function WLog($LogText)
+{ 
+    CreateFolder $LogPath
+    $TimeStamp = "$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S") >> "
+    $Log = "$TimeStamp$LogText"
+    $Log | Out-File -FilePath $LogPath$LogFile -Append
+    Write-Output $Log  
+}
+
+function CreateFolder($FolderName)
+{
+    if (Test-Path $FolderName) {
+        # directory exists
+    } else {
+        New-Item $FolderName -ItemType Directory
+        Write-Host "Log directory ' + $LogPath + ' created"
+    }
+}
+
+$LogFile = "$(Get-Date -UFormat "%Y%m%d_%H%M%S").txt"
+$LogPath = "C:\IBOTS\PowerShell Scripts\PS Script Logs\"
+
+WLog "test"
+
+WLog "test2"
+WLog
